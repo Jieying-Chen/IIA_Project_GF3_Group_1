7b288a70615092676afedf063ec6fabbacb88dfd
unknown <zhaojw1024@gmail.com>
2022-06-05 19:34:26 +0100

    ldpc receiver

--- utils/decode.py
+++ utils/decode.py
@@ -1,4 +1,5 @@
 import numpy as np
+from utils import ldpc
 
 def qpsk_decode(input): 
     """Map an narray of complex numbers to corresponding binary bits pairs"""
@@ -14,3 +15,32 @@ def qpsk_decode(input):
 def bpsk_decode(input):
 
     return (input > 0).astype(int)
+
+def channel_noise(H,received_known,dft_length):
+    received_known = np.reshape(received_known, (-1, dft_length))
+    received_fft = np.fft.fft(received_known,axis = 1)
+    print(received_fft.shape)
+    noise = np.divide(received_fft,H)   #a matrix of complex noise
+    print(noise.shape)
+    sigma = np.std(np.reshape(noise,(-1)))
+    return sigma
+
+
+def ldpc_decode(deconvolved, H, sigma,spb):     #takes channel estimation at info subcarriers only
+    c = ldpc.code()
+    L = np.array([])
+    for row in np.reshape(deconvolved,(-1,spb)):
+        L1 = H*np.conj(H)* np.sqrt(2)*np.imag(row)/sigma
+        L2 = H*np.conj(H)* np.sqrt(2)*np.real(row)/sigma
+        L_stacked  = np.vstack((L1,L2)).reshape((-1,),order='F')
+        L = np.append(L,L_stacked)
+
+    #decode for each c.Nv bits
+    decoded = np.array([])
+    iteration = np.array([])
+    for row in np.reshape(L,(-1,c.Nv)):
+        app,iter = c.decode(row)
+        decoded = np.append(decoded,app[:c.K])
+        iteration = np.append(iteration,iter)
+
+    return decoded,iteration
